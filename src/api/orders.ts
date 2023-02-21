607abcbc0edda618db2907d490750bf30dc2258e
aland <aandreenko1997@gmail.com>
2023-02-22 00:44:01 +0700

    update order api method

--- src/api/orders.ts
+++ src/api/orders.ts
@@ -1,7 +1,7 @@
 import { Order } from "../models/Order"
 import { Time } from "../models/Time"
 import { userId } from "../stores/user"
-import { baseApiFetch } from "./base"
+import { baseApiFetch, type NoId } from "./base"
 
 function toOrder(raw: any) {
   return new Order({
@@ -11,17 +11,28 @@ function toOrder(raw: any) {
   })
 }
 
-function byDate(ord1, ord2) {
+function byDate(ord1: Order, ord2: Order) {
   return ord1.date.getFullYear() -  ord2.date.getFullYear()
   || ord1.date.getMonth() - ord2.date.getMonth()
   || ord1.date.getDate() - ord2.date.getDate()
   || ord1.time.toMinutes() - ord2.time.toMinutes()
 }
 
-export async function createOrder(order) {
-  return await baseApiFetch('/orders', 'POST', {
-    order    
-  })
+export interface OrderForm {
+  year: number,
+  month: number,
+  day: number,
+  time: Time,
+  userId: string,
+  serviceIds: string[]
+}
+
+export async function createOrder(order: OrderForm) {
+  return await baseApiFetch('/orders', 'POST', { order })
+}
+
+export async function updateOrder(order: Order) {
+  return await baseApiFetch('/orders/' + order.id, 'PUT', { order })
 }
 
 export async function getOrders(year: number, month: number, day?: number) {
@@ -46,10 +57,20 @@ export async function cancelOrder(orderId: string) {
   return await baseApiFetch('/orders/' + orderId, 'DELETE');
 }
 
-export async function getSlots(year: number, month: number, duration: number) : Promise<{[key: string]: Time[]}>{
-  const slots = await baseApiFetch('/orders/slots', 'POST', {
-    year, month, duration
-  }).then(res => res.json())
+export async function getSlots(
+  year: number,
+  month: number,
+  duration: number,
+  editOrderId?: string
+) : Promise<{[key: string]: Time[]}> {
+
+  const body = { year, month, duration } as any
+  if (editOrderId) {
+    body.orderId = editOrderId
+  }
+  
+  const slots = await baseApiFetch('/orders/slots', 'POST', body)
+    .then(res => res.json())
   
   return Object.keys(slots).reduce((acc, day) => {
     acc[day] = slots[day].map(Time.fromMinutes)
