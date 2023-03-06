ee678d1928904bc31ad05fda1baa90d635596bcb
aland <aandreenko1997@gmail.com>
2023-03-06 14:32:06 +0700

    phone and name not required

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,6 @@
   import Loader from "./components/Loader.svelte";
   import Route from "./components/router/Route.svelte";
   import Router from "./components/router/Router.svelte"
-  import UserForm from "./components/UserForm.svelte";
   import type { User } from "./models/User";
   import MainMenu from "./screens/MainMenu.svelte"
   import NewCategory from "./screens/NewCategory.svelte";
@@ -23,36 +22,26 @@
     return user
   }
 
-  let userPromise = getUser(userId).then(updateAdmin)
-
-  function submitHandler({ detail: user }: CustomEvent<User>) {
-    userPromise = updateUser(user)
-      .then(() => getUser(userId))
-      .then(updateAdmin)
-  }
-
+  let userPromise = getUser(userId)
+    .then(updateAdmin)
 </script>
 
 <div class="container">
   {#await userPromise}
     <Loader />
-  {:then user}
-    {#if user?.phone}
-      <Router>
-        <Route component={MainMenu} path={RoutePath.MainMenu} />
-        <Route component={OrderList} path={RoutePath.MyOrders} />
-        <Route component={NewOrder} path={RoutePath.NewOrder} />
-        <Route component={Profile} path={RoutePath.Profile} />
-        {#if $isAdmin}
-          <Route component={ServicesList} path={RoutePath.ServicesList} />
-          <Route component={NewService} path={RoutePath.NewService} />
-          <Route component={NewCategory} path={RoutePath.NewCategory} />
-          <Route component={Shedulle} path={RoutePath.Shedulle} />
-        {/if}
-      </Router>
-      {:else}
-      <UserForm on:submit={submitHandler}/>
-    {/if}
+  {:then}
+    <Router>
+      <Route component={MainMenu} path={RoutePath.MainMenu} />
+      <Route component={OrderList} path={RoutePath.MyOrders} />
+      <Route component={NewOrder} path={RoutePath.NewOrder} />
+      <Route component={Profile} path={RoutePath.Profile} />
+      {#if $isAdmin}
+        <Route component={ServicesList} path={RoutePath.ServicesList} />
+        <Route component={NewService} path={RoutePath.NewService} />
+        <Route component={NewCategory} path={RoutePath.NewCategory} />
+        <Route component={Shedulle} path={RoutePath.Shedulle} />
+      {/if}
+    </Router>
   {:catch}
       <div>Ссылка не действительна</div>
   {/await}
