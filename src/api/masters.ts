cb3d02d8f316c1e4696240bdf3877485b21069d9
aland <aandreenko1997@gmail.com>
2023-03-10 17:13:00 +0700

    get/update master info

--- src/api/masters.ts
+++ src/api/masters.ts
@@ -1,7 +1,13 @@
+import { MasterInfo } from "../models/MasterInfo";
 import { baseApiFetch } from "./base";
 
-export async function getAddress() : Promise<String> {
-  const res = await baseApiFetch('/masters/address')
+export async function getInfo() : Promise<MasterInfo> {
+  const res = await baseApiFetch('/masters/info')
   const json = await res.json()
-  return json.address as String
+  return new MasterInfo({...json})
+}
+
+export async function updateInfo(info : MasterInfo) {
+  const res = await baseApiFetch('/masters/info', 'PUT', {...info})
+  return await res.json()
 }
