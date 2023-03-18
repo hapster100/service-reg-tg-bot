269e583652712ac691823bb713d1c2236d3ad575
aland <aandreenko1997@gmail.com>
2023-03-18 22:10:36 +0700

    edit master shedulle step

--- src/models/MasterInfo.ts
+++ src/models/MasterInfo.ts
@@ -1,12 +1,15 @@
 export class MasterInfo {
   readonly address: string
   readonly successImageUrl: string
+  readonly shedulleStep: number
 
   constructor({
     address,
     successImageUrl,
+    shedulleStep
   }) {
     this.address = address || ''
     this.successImageUrl = successImageUrl || ''
+    this.shedulleStep = shedulleStep || 15
   }
 }
--- src/screens/MasterInfo.svelte
+++ src/screens/MasterInfo.svelte
@@ -4,17 +4,22 @@
   import BackToMenu from "../components/BackToMenu.svelte";
   import ImageUrlInput from "../components/ImageUrlInput.svelte";
   import Loader from "../components/Loader.svelte";
+  import Time from "../components/Time.svelte";
   import { MasterInfo } from "../models/MasterInfo";
   
+  const STEP = 15
+
   let process = true
   let imageUrl = ''
   let address = ''
+  let shedulleStep = 15
 
   const update = async () => {
     process = true
     await getInfo().then(info => {
       imageUrl = info.successImageUrl
       address = info.address
+      shedulleStep = info.shedulleStep
       process = false
     })
   }
@@ -22,7 +27,11 @@
 
   const handle = async (e: SubmitEvent) => {
     e.preventDefault()
-    const info = new MasterInfo({address, successImageUrl: imageUrl})
+    const info = new MasterInfo({
+      address,
+      successImageUrl: imageUrl,
+      shedulleStep
+    })
     process = true
     await updateInfo(info)
     await update()
@@ -44,6 +53,14 @@
     <div class="form-field">
       <label for="image">Картинка в уведомлении</label>
       <ImageUrlInput bind:imageUrl />
+    </div>
+    <div class="form-field">
+      <label for="time-step">Шаг в расписании</label>
+      <input type="range" name="duration" 
+        min={STEP} max={STEP*4} step={STEP}
+        bind:value={shedulleStep}
+      />
+      <Time duration={shedulleStep} />
     </div>
     <button type="submit">Сохранить</button>
   </form>
